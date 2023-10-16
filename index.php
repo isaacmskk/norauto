493cf498b04a626267ffb1ae71db611870e499c2
isaac <isaac@prueba.com>
2023-10-16 19:13:10 +0200

    comiteado

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 
-    echo 'esaa';
+    if (isset($_GET['controller'])){
+        echo'quieres realizar una accion sobre '.$_GET['controller'];
+        if(isset($_GET['action'])){
+            echo'quieres realizar una accion sobre '.$_GET['action'];
+        }
+    }else{
+        echo'No me has pasado controller';
+    }
+
 ?>
