78f6400079f4d7d588cf73cb9fc39d82c5bb9983
isaac <isaac@prueba.com>
2024-02-12 15:44:01 +0100

    localstorage añadido en filtro

--- javascript/filtroproductos.js
+++ javascript/filtroproductos.js
@@ -1,22 +1,49 @@
 const categoryCheckboxes = document.querySelectorAll('.category-checkbox');
 
+// Al cargar la página, verifica si hay selecciones almacenadas en localStorage
+document.addEventListener('DOMContentLoaded', function () {
+    const storedSelections = localStorage.getItem('selectedCategories');
+
+    if (storedSelections) {
+        const selectedCategories = storedSelections.split(',');
+        selectedCategories.forEach(category => {
+            const checkbox = document.querySelector(`.category-checkbox[value="${category}"]`);
+            if (checkbox) {
+                checkbox.checked = true;
+            }
+        });
+
+        // Actualiza los productos basándose en las categorías seleccionadas almacenadas
+        updateProducts();
+    }
+});
+
 categoryCheckboxes.forEach(checkbox => {
-    checkbox.addEventListener('click', updateProducts);
+    checkbox.addEventListener('click', function () {
+        updateProducts();
+
+        // Almacena las categorías seleccionadas en localStorage
+        const selectedCategories = Array.from(categoryCheckboxes)
+            .filter(checkbox => checkbox.checked)
+            .map(checkbox => checkbox.value);
+
+        localStorage.setItem('selectedCategories', selectedCategories.join(','));
+    });
 });
 
 function updateProducts() {
-    const selectedCategorias = Array.from(categoryCheckboxes)
+    const selectedCategories = Array.from(categoryCheckboxes)
         .filter(checkbox => checkbox.checked)
         .map(checkbox => checkbox.value);
 
     const allProducts = document.querySelectorAll('.col-12.col-lg-3');
 
-    if (selectedCategorias.length === 0) {
+    if (selectedCategories.length === 0) {
         allProducts.forEach(product => product.style.display = 'block');
     } else {
         allProducts.forEach(product => {
-            const categoriaProducto = product.getAttribute('data-categoria');
-            const isVisible = selectedCategorias.includes(categoriaProducto);
+            const productCategory = product.getAttribute('data-categoria');
+            const isVisible = selectedCategories.includes(productCategory);
             product.style.display = isVisible ? 'block' : 'none';
         });
     }
@@ -25,4 +52,7 @@ function updateProducts() {
 function resetFilters() {
     categoryCheckboxes.forEach(checkbox => checkbox.checked = false);
     updateProducts();
+
+    // Elimina las categorías seleccionadas almacenadas en localStorage al restablecer los filtros
+    localStorage.removeItem('selectedCategories');
 }
